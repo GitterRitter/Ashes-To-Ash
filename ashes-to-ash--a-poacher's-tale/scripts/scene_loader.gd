407feada445119cf30b5c538cc82455f64a41d3b
Moritz Maile <ravestudios@web.de>
2025-08-23 20:26:48 +0200

    feat: added buttons #5

--- ashes-to-ash--a-poacher's-tale/scripts/scene_loader.gd
+++ ashes-to-ash--a-poacher's-tale/scripts/scene_loader.gd
@@ -1,7 +1,4 @@
 extends Node
 
-func close_scene() -> void:
-	get_tree().quit()
-
 func change_scene(path : String) -> void:
 	get_tree().change_scene_to_file(path)
---
+++ ashes-to-ash--a-poacher's-tale/scripts/start_scene.gd
@@ -0,0 +1,29 @@
+extends Node
+
+@export var continue_button : Button
+@export var new_game_button : Button
+@export var settings_button : Button
+@export var credits_button : Button
+@export var exit_button : Button
+
+func _ready():
+	continue_button.pressed.connect(on_continue_button_pressed)
+	new_game_button.pressed.connect(on_new_game_button_pressed)
+	settings_button.pressed.connect(on_settings_button_pressed)
+	credits_button.pressed.connect(on_credits_button_pressed)
+	exit_button.pressed.connect(on_exit_button_pressed)
+
+func on_continue_button_pressed():
+	pass
+
+func on_new_game_button_pressed():
+	SceneLoader.change_scene("res://scenes/new_game.tscn")
+
+func on_settings_button_pressed():
+	SceneLoader.change_scene("res://scenes/settings.tscn")
+
+func on_credits_button_pressed():
+	SceneLoader.change_scene("res://scenes/credits.tscn")
+
+func on_exit_button_pressed():
+	get_tree().quit()
