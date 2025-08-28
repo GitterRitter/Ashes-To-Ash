c748073d640b0c36f4830a1b58662202a57ad1db
Moritz Maile <ravestudios@web.de>
2025-08-28 10:39:34 +0200

    feat: added methods for creatings configs #6

--- ashes-to-ash--a-poacher's-tale/scripts/settings.gd
+++ ashes-to-ash--a-poacher's-tale/scripts/settings.gd
@@ -1,20 +1,66 @@
 extends Node
 
-@export var audio_button : Button
-@export var video_button : Button
-@export var controls_button : Button
+# Sound Settings
+@export var master_slide : HSlider
+@export var sfx_slide : HSlider
+@export var music_slide : HSlider
+
+# Visual Settings
+@export var screen_mode_menu : OptionButton
+@export var resolution_menu : OptionButton
+@export var vertical_sync_button : CheckButton
+@export var max_fps_line_edit : LineEdit
+
+# Control Settings
+
+
+var master_volume : int
+var sfx_volume : int
+var music_volume : int
+
+var screen_mode : String
+var resolution : String
+var vertical_sync_on : bool
+var max_fps : int
+
+var keybinds : Dictionary
+
 
 func _ready():
-	audio_button.pressed.connect(on_audio_button_pressed)
-	video_button.pressed.connect(on_new_game_button_pressed)
-	controls_button.pressed.connect(on_settings_button_pressed)
+	pass
 
+func create_default_config() -> Dictionary:
+	var keybinds = {
+		"up": "w",
+		"down" : "s",
+		"left" : "a",
+		"right" : "d",
+		"jump" : "space"
+	};
+	var config = {
+		"master_volume" : 1,
+		"sfx_volume" : 1,
+		"music_volume" : 1,
+		"screen_mode" : "Fullscreen",
+		"resolution" : "1920x1080",
+		"vertical_sync_on" : false,
+		"max_fps" : 60,
+		"keybinds" : keybinds
+	}
+	return config;
 
-func on_audio_button_pressed():
-	SceneLoader.change_scene("res://scenes/settings/audio.tscn")
+func load_config(config : Dictionary):
+	master_volume = config["master_volume"];
+	sfx_volume = config["sfx_volume"];
+	music_volume = config["music_volume"];
+	screen_mode = config["master_volume"];
+	resolution = config["resolution"];
+	vertical_sync_on = config["vertical_sync_on"];
+	max_fps = config["max_fps"];
+	keybinds = config["keybinds"];
 
-func on_new_game_button_pressed():
-	SceneLoader.change_scene("res://scenes/settings/video.tscn")
-
-func on_settings_button_pressed():
-	SceneLoader.change_scene("res://scenes/settings/controls.tscn")
+func get_config_from_path(path : String) -> Dictionary:
+	var config_string = String.get_file(path);
+	var config = {};
+	return config;
+	
